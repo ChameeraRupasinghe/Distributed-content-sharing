328b3684f0ecbcbfc17fbb78728470c69622941f
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2020-02-08 15:29:25 +0530

    update some code

--- src/com/distributed/Main.java
+++ src/com/distributed/Main.java
@@ -3,6 +3,7 @@ package com.distributed;
 import com.distributed.request.RegisterRequestMessage;
 import com.distributed.request.LeaveRequestMessage;
 import com.distributed.request.RequestMessage;
+import com.distributed.request.SearchRequestMessage;
 
 import java.io.IOException;
 import java.net.*;
@@ -11,6 +12,7 @@ import java.util.Scanner;
 public class Main {
 
     static InetAddress ipAddress;
+    static int listeningPort;
 //    static String userName;
 
     static DatagramSocket socket;
@@ -37,18 +39,40 @@ public class Main {
 
             //Send registration request
             RequestMessage regRequestMessage = new RegisterRequestMessage(ipAddress, listeningPort, userName);
-            DatagramPacket messagePacket = regRequestMessage.getDatagramPacket(Config.BS_ADDRESS, Config.BS_PORT);
-            socket.send(messagePacket);
+            DatagramPacket regMessagePacket = regRequestMessage.getDatagramPacket(Config.BS_ADDRESS, Config.BS_PORT);
+            socket.send(regMessagePacket);
 
-//            TODO: JOIN Request to other nodes (get the deatils from the bootstrap
+//            TODO: JOIN Request to other nodes (get the details from the bootstrap
 
 //            TODO: SEARCH (get input from the terminal)
+            while (true) {
+                // take input and send the packet
+                System.out.println("\nSelect option : ");
+                System.out.println("1: Search");
+                System.out.println("2: Disconnect");
+                int option = Integer.parseInt(scanner.nextLine().trim());
+                switch (option) {
+                    case 1:
+                        System.out.println("Enter the file name: ");
+                        handleSearch(scanner.nextLine().trim());
+                        break;
+
+                    case 2:
+                        System.out.println("Disconnecting this node");
+                        handleDisconnect();
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Please enter a valid input");
+                        break;
+                }
+            }
 
 //            TODO: Download
 
 //            TODO: LEAVE
-            RequestMessage leaveRequestMessage = new LeaveRequestMessage(ipAddress, listeningPort, userName);
-            DatagramPacket messPacket = leaveRequestMessage.getDatagramPacket(Config., destinationPort)
+//            RequestMessage leaveRequestMessage = new LeaveRequestMessage(ipAddress, listeningPort, userName);
+//            DatagramPacket messPacket = leaveRequestMessage.getDatagramPacket(Config., destinationPort)
 
         } catch (UnknownHostException e) {
             e.printStackTrace();
@@ -62,5 +86,13 @@ public class Main {
     static void initializeListener(int listeningPort) {
         Listener listener = new Listener(listeningPort);
         listener.start();
+    }
+
+    static void handleSearch(String filename){
+        RequestMessage searchRequestMessage = new SearchRequestMessage(ipAddress, );
+    }
+
+    static void handleDisconnect(){
+
     }
 }
---
+++ src/com/distributed/request/SearchRequestMessage.java
@@ -0,0 +1,34 @@
+package com.distributed.request;
+
+import java.net.InetAddress;
+
+public class SearchRequestMessage extends RequestMessage{
+
+    private static final String TYPE = "SER";
+    private InetAddress myAddress;
+    private int myPort;
+
+    private String fileName;
+    private int hops;
+
+
+    public SearchRequestMessage(InetAddress myAddress, int myPort, String fileName, int hops) {
+        this.myAddress = myAddress;
+        this.myPort = myPort;
+        this.fileName = fileName;
+        this.hops = hops;
+    }
+
+    @Override
+    public String getMessageString() {
+        StringBuilder messageBuilder = new StringBuilder();
+        messageBuilder.append(TYPE).append(" ");
+        messageBuilder.append(myAddress.getHostAddress().trim()).append(" ");
+        messageBuilder.append(myPort).append(" ");
+        messageBuilder.append(hops);
+
+        String messageLength = String.format("%04d", messageBuilder.length() + 5);
+
+        return messageLength + " " + messageBuilder.toString();
+    }
+}
